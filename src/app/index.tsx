299bc73630eeb0a51e1ce83dfa21c9282de004c0
Navaneeth P P <appunavaneeth0@gmail.com>
2026-05-23 10:49:00 +0530

    adding splash screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,5 @@
 import { View, Text } from "react-native";
 
 export default function Home() {
-  return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-3xl font-bold">
-        REVOPZ Mobile 🚀
-      </Text>
-    </View>
-  );
+  return <Text>Hi</Text>;
 }
